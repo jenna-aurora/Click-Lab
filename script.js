c3d1e8af4010b20932025f6aaa6dbb4487ced97c
jenna-aurora <jenna.a.cardenas@outlook.com>
2026-08-20 14:05:36 -0500

    Hint, click the button. closes #4

--- script.js
+++ script.js
@@ -8,10 +8,11 @@ let quizReaction = document.getElementById("reaction");
 let answer0 = document.getElementById("answer-0");
 let answer1 = document.getElementById("answer-1");
 let answer2 = document.getElementById("answer-2");
+let hintButton = document.getElementById("hint-button");
 
 answer0.addEventListener("click", function () {
 	quizReaction.textContent = "Erroneous - her location wasn't what the interrogators wanted.";
-	document.body.style.backgroundColor = "rebeccapurple";
+	document.body.style.backgroundColor = "crimson";
 	setTimeout(() => {
 		document.body.style.backgroundColor = "";
 	}, 300);
@@ -19,14 +20,17 @@ answer0.addEventListener("click", function () {
 
 answer1.addEventListener("click", function () {
 	quizReaction.textContent = "Misguided - her Sparrow Instructors weren't the focus of the interrogation.";
-	document.body.style.backgroundColor = "rebeccapurple";
+	document.body.style.backgroundColor = "crimson";
 	setTimeOut(() => {
 		document.body.style.backgroundColor = "";
 	}, 300);
 });		
 
 answer2.addEventListener("click", function () {
-	quizReaction.textContent = "Defenitive, they pressured her to reveal Boucher, the mole inside Russian Intelligence.";"
+	quizReaction.textContent = "Defenitive, they pressured her to reveal Boucher, the mole inside Russian Intelligence.";
 	document.body.style.backgroundColor = "";
-		"Correct!"
 }); 
+// -->Hint button
+hintButton.addEventListener("click", function () {
+	quizReaction.textContet = "Hint: They wanted the name of a mole inside the American intelligence community.";
+});
